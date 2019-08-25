af385a4037749f64057b81b7f8993bfb5aa26b0d
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-08-25 15:08:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,38 @@
+#turn 
+def turn(position)
+  puts "Please enter 1-9: "
+  position = position.chomp
+end
+
+#move 
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board,position,char = 'X')
+  board[position] = char
+end 
+
+#position_taken & valid_move
+def position_taken?(board, index)
+ if board[index] == nil || board[index] == ""
+    return false
+ elsif board[index] == " "
+    return false
+ elsif board[index] == "X" || board[index] == "O"
+    return true
+ end
+end
+
+def valid_move?(board,index)
+  if board[index] == nil 
+    return false 
+    elsif !position_taken?(board, index)
+    return true 
+  end
+end
+
+
 #display_board
 def display_board
   puts "   |   |   "
@@ -19,39 +54,10 @@ end
 board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
 
-#position_taken & valid_move
-def position_taken?(board, index)
- if board[index] == nil || board[index] == ""
-    return false
- elsif board[index] == " "
-    return false
- elsif board[index] == "X" || board[index] == "O"
-    return true
- end
-end
 
-def valid_move?(board,index)
-  if board[index] == nil 
-    return false 
-    elsif !position_taken?(board, index)
-    return true 
-  end
-end
 
-#move 
-def input_to_index(input)
-  input.to_i - 1
-end
 
-def move(board,position,char = 'X')
-  board[position] = char
-end 
 
-#turn 
-def turn(position)
-  puts "Please enter 1-9: "
-  position = position.chomp
-end
 
 
 
